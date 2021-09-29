f8b8cc696a5612f237a5d9f2a22780347ce7958c
Aurian-Nogues <aurian.nogues@gmail.com>
2021-09-29 21:25:02 +0100

    test

--- execute.sh
+++ execute.sh
@@ -15,5 +15,5 @@ sudo xset s noblank
 
 
 # commands to start and continue running simultaneously display manager and bluetooth receiver
-/home/pi/frame_pi/venv/bin/python3 /home/pi/frame_pi/display/display_manager.py &
+/home/pi/switchcraft_pi/venv/bin/python3 /home/pi/switchcraft_pi/display/display_manager.py &
 #sudo /home/pi/frame_pi/venv/bin/python3 /home/pi/frame_pi/main.py
